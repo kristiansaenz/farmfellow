2132635af919e711c8303d741b88d5f13a97fe8e
krisaenz <ksaenz@brikit.com>
2024-04-30 15:25:43 -0700

    update packages + cleanup some pages

---
+++ src/app/booths/[id]/page.tsx
@@ -0,0 +1,11 @@
+import {Container} from '@mantine/core'
+
+//TODO: Use react query to fetch booth data by id from the API
+
+export default function Page() {
+  return (
+    <Container>
+      <h1>Booth Page</h1>
+    </Container>
+  )
+}
